44547fbaa46c3c068597fa23c42cd9b6efa7502b
nitish kumar raj <nitishksingh123@gmail.com>
2017-05-10 23:09:32 +0530

    Create additems.php

---
+++ additems/additems.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form method="GET" action="1.php">
+    ITEMS ID:<input type="text" name="itm_id"></input>
+	<br>
+    ITEMS NAME:<input type="text" name="itm_name"></input>
+	<br>
+	BRAND ID:<input type="text" name="brand_id"></input>
+	<br>
+	BRAND NAME:<input type="text" name="brand_name"></input>
+	<br>
+	PRODUCT NO WITH SIZE:<input type="text" name="pnwsize"></input>
+	<br>
+    SELLING PRICE:<input type="text" name="sprice"></input>
+	<br>
+    COST PRICE:<input type="text" name="cprice"></input>
+	<br>
+	<input type="submit" name="submit">
+</form>
+
